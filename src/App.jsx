b2a139e2f6516b081a358d8d2cca8497035d0f92
Bharath <bharath19h142@gmail.com>
2024-06-29 04:08:52 -0700

    uploading files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Card from './Card';
+// import { Card } from './Card';
+
+function App() {
+  return (
+  <div>
+    <div className="text-center">
+      <h2 className="title">Price Card</h2>
+    </div>
+
+    <div className="row" >
+      <Card amt="0" name="free"/>
+      <Card amt="9" name="plus"/>
+      <Card amt="49" name="pro"/>
+      
+    </div>
+  </div>
+
+  );
+}
+
+export default App;
